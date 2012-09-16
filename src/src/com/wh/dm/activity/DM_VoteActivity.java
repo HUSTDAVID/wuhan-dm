1a26d148f58192b7d28d478b6d6a4351d2212d28
Erik-qd <275920485@qq.com>
2012-09-16 18:58:03 +0800

    voting process page

---
+++ src/src/com/wh/dm/activity/DM_VoteActivity.java
@@ -0,0 +1,17 @@
+
+package com.wh.dm.activity;
+
+import com.wh.dm.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DM_VoteActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+    }
+}
