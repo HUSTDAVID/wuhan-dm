219d6d70af7a91d1a53b77f516d7926470a44179
Erik-qd <275920485@qq.com>
2012-09-20 21:01:15 +0800

    vote changed，after the click，can find the right page

--- src/src/com/wh/dm/activity/DM_MZine1Activity.java
+++ src/src/com/wh/dm/activity/DM_MZine1Activity.java
@@ -37,7 +37,7 @@ public class DM_MZine1Activity extends Activity implements OnClickListener {
 
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_dm_mzine1_new);
+        setContentView(R.layout.activity_dm_mzine1);
 
         initViews();
     }
--- src/src/com/wh/dm/activity/DM_Vote2Activity.java
+++ src/src/com/wh/dm/activity/DM_Vote2Activity.java
@@ -4,6 +4,7 @@ package com.wh.dm.activity;
 import com.wh.dm.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,6 +12,9 @@ import android.widget.Button;
 
 public class DM_Vote2Activity extends Activity {
     private Button btn_close;
+    private Button votebutton1;
+    private Button votebutton2;
+    private Button votebutton3;
 
     /** Called when the activity is first created. */
     @Override
@@ -18,7 +22,39 @@ public class DM_Vote2Activity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote2);
+
         init();
+
+        votebutton1 = (Button) findViewById(R.id.vote_button1);
+        votebutton1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(DM_Vote2Activity.this, DM_VoteWatchResult.class);
+                startActivity(intent);
+
+            }
+        });
+        votebutton2 = (Button) findViewById(R.id.vote_button2);
+        votebutton2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(DM_Vote2Activity.this, DM_VoteWatchResult.class);
+                startActivity(intent);
+
+            }
+        });
+        votebutton3 = (Button) findViewById(R.id.vote_button3);
+        votebutton3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(DM_Vote2Activity.this, DM_VoteWatchResult.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     public void init() {
