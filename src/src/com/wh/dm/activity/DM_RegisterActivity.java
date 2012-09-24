76408f5e0c8bcc265b9ffaabc4f6deca0f475150
luguanquan <757111858@qq.com>
2012-09-24 22:04:59 +0800

    add register page

---
+++ src/src/com/wh/dm/activity/DM_RegisterActivity.java
@@ -0,0 +1,44 @@
+
+package com.wh.dm.activity;
+
+import com.wh.dm.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class DM_RegisterActivity extends Activity {
+
+    EditText edtEmail;
+    EditText edtPasswd;
+    CheckBox cbShowPw;
+    Button btnRigester;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_register);
+
+        initViews();
+    }
+
+    private void initViews() {
+
+        // init header
+        TextView txtHeader = (TextView) findViewById(R.id.txt_header_title2);
+        txtHeader.setText(getResources().getString(R.string.register));
+
+        // init view
+        edtEmail = (EditText) findViewById(R.id.edt_register_emial);
+        edtPasswd = (EditText) findViewById(R.id.edt_register_passwd);
+        cbShowPw = (CheckBox) findViewById(R.id.cb_show_passwd);
+        btnRigester = (Button) findViewById(R.id.btn_register_send);
+    }
+
+}
--- src/src/com/wh/dm/activity/MainActivity.java
+++ src/src/com/wh/dm/activity/MainActivity.java
@@ -347,7 +347,7 @@ public class MainActivity extends Activity {
                     case 5:
 
                         Intent intent_pic_txt3 = new Intent(MainActivity.this,
-                                DM_FeedbackActivity.class);
+                                DM_RegisterActivity.class);
                         bundle.putInt("dm", DM_PICS_TXT3);
                         intent_pic_txt3.putExtras(bundle);
                         startActivity(intent_pic_txt3);
